a48045cf113bffacf0ea34995c7ad604497a91a9
ruben <rgerritse95@gmail.com>
2018-05-03 20:15:50 +0200

    data preprocessing

--- Practical_2/main.py
+++ Practical_2/main.py
@@ -12,22 +12,44 @@ num_epochs = 50
 batch_size = 512
 window = 2
 
+# %% Data selection
+word_set = set()
+
+with open("data/lst/lst.gold.candidates") as fc:
+    for line in fc.readlines():
+        split_line = line.strip().split('::')
+        word_set.add(split_line[0].split('.')[0])
+        for word in split_line[1].split(';'):
+            if len(word.split()) == 1:
+                word_set.add(word)
+
+with open("data/lst/lst_test.preprocessed") as fp:
+    for line in fp.readlines():
+        split_line = line.strip().split()
+        i = int(split_line[2])
+        sentence = split_line[3:]
+        for j in range(i-window, i+window+1):
+            if j>=0 and j<len(sentence):
+                word_set.add(sentence[j])
+
 # %% Dataset creation
 with open("data/hansards/training.en") as f:
-    tokens = f.read().split()
+    tokens = [line.split() for line in f.readlines()]
 
-vocab = list(set(tokens))
+vocab = list(set([token for sentence in tokens for token in sentence]))
 vocab_size = len(vocab)
 w2i = {k: v for v, k in enumerate(vocab)}
 i2w = {v: k for v, k in enumerate(vocab)}
 
 data = []
 labels = []
-for i in range(len(tokens)):
-    for j in range(i-window, i+window+1):
-        if i != j and j>=0 and j<len(tokens):
-            data.append(w2i[tokens[j]])
-            labels.append(w2i[tokens[i]])
+for sentence in tokens:
+    for i in range(len(sentence)):
+        for j in range(i-window, i+window+1):
+            if i != j and j>=0 and j<len(sentence):
+                if sentence[i] in word_set:
+                    data.append(w2i[sentence[i]])
+                    labels.append(w2i[sentence[j]])
 
 # %% Network Definition
 class Net(nn.Module):
@@ -65,3 +87,5 @@ for epoch in range(num_epochs):
 # %% Get embeddings
 embeddings = net.embeddings.weight
 vocab_size
+
+#%%
