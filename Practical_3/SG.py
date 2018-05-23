42262f65b95588babb0d2d703d035e2e390805bb
Matthew van Rijn <matthew.van.rijn@gmail.com>
2018-05-23 15:49:37 +0200

    Skipgram for seneval

---
+++ Practical_3/SG.py
@@ -0,0 +1,76 @@
+# %% Imports
+import models, torch, os, time
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+import utils, importlib
+importlib.reload(utils)
+from utils import create_vocab, create_SG_dataset, save_as_glove
+
+# %% Parameters
+model_name = 'SGeuroparl'
+dataset = '../Practical_2/data/hansards/training.en'
+embedding_dest = 'SentEval/pretrained/SG.txt'
+embed_size = 128
+learning_rate = 0.001
+num_epochs = 100
+batch_size = 100
+window_size = 5
+top_size = 10000 # Top n words to use for training, all other words are mapped to <unk>, use None if you do not want to map any word to <unk>
+
+# %% Construct vocabulary and load dataset
+vocab, vocab_size, w2i, i2w = create_vocab(top_size, dataset, [], 'stopwords_english')
+targets, contexts = create_SG_dataset(dataset, window_size, w2i)
+
+# %% Train
+train_data = torch.utils.data.TensorDataset(targets, contexts)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size, shuffle=True)
+
+net = models.SkipGram(vocab_size, embed_size).cuda()
+loss_fn = nn.CrossEntropyLoss()
+opt = torch.optim.Adam(net.parameters(), learning_rate)
+saved_epoch = 0
+train_errors = []
+
+# Check for saved checkpoint
+checkpoints = [cp for cp in sorted(os.listdir('checkpoints')) if '-'.join(cp.split('-')[:-1]) == model_name]
+if checkpoints:
+    state = torch.load('checkpoints/{}'.format(checkpoints[-1]))
+    saved_epoch = state['epoch'] + 1
+    lst_scores = state['lst_err']
+    train_errors = state['train_err']
+    net.load_state_dict(state['state_dict'])
+    opt.load_state_dict(state['optimizer'])
+
+for epoch in range(saved_epoch, num_epochs):
+    start_time = time.time()
+    num_batches = len(train_loader)
+    total_loss = 0
+    for batch, (target, context) in enumerate(train_loader):
+        opt.zero_grad()
+        output = net(target)
+
+        loss = loss_fn(output, context)
+        total_loss += loss.data.item()
+        loss.backward()
+        opt.step()
+
+        pace = (batch+1)/(time.time() - start_time)
+        print('\r[Epoch {:03d}/{:03d}] Batch {:06d}/{:06d} [{:.1f}/s] '.format(epoch+1, num_epochs, batch+1, num_batches, pace), end='')
+
+    # Calculate LST score
+    total_loss /= len(train_loader)
+    print('Time: {:.1f}s Loss: {:.3f}'.format(time.time() - start_time, total_loss))
+
+    train_errors.append(total_loss)
+
+    # Save checkpoint
+    state = {
+        'epoch': epoch,
+        'state_dict': net.state_dict(),
+        'optimizer': opt.state_dict(),
+        'train_err': train_errors
+    }
+    torch.save(state, 'checkpoints/{}-{}'.format(model_name, epoch))
+
+# %% Save embeddings
+save_as_glove(embedding_dest, net.embeddings.weight, i2w)
---
+++ Practical_3/models.py
@@ -0,0 +1,135 @@
+import torch.nn as nn
+import torch, sys
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+class SkipGram(nn.Module):
+    def __init__(self, vocab_size, embed_size):
+        super(SkipGram, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.fc1 = nn.Linear(embed_size, vocab_size)
+
+    def forward(self, data):
+        out = self.embeddings(data)
+        out = self.fc1(out)
+        return out
+
+class BayesianEncoder(nn.Module):
+    def __init__(self, vocab_size, embed_size, window):
+        super(BayesianEncoder, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.relu = nn.ReLU()
+        self.softplus = nn.Softplus()
+        self.fc1 = nn.Linear(2 * embed_size, embed_size)
+        self.fc2 = nn.Linear(2 * embed_size, embed_size)
+        self.m = nn.Linear(2 * embed_size, 2 * embed_size)
+        self.window = window
+
+    def forward(self, target, contexts):
+        target_emb = self.embeddings(target)
+        target_emb = target_emb.repeat(1, 2 * self.window, 1)
+        contexts_emb = self.embeddings(contexts)
+        cat_emb = torch.cat((target_emb, contexts_emb), 2)
+        proj_emb = self.m(cat_emb)
+        relu_emb = self.relu(proj_emb)
+        sum_emb = torch.sum(relu_emb, 1)
+        mu_emb  = self.fc1(sum_emb)
+        sigma_emb = self.softplus(self.fc2(sum_emb))
+
+        return mu_emb, sigma_emb
+
+class BayesianDecoder(nn.Module):
+    def __init__(self, vocab_size, embed_size):
+        super(BayesianDecoder, self).__init__()
+        self.affine = nn.Linear(embed_size, vocab_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, z):
+        return self.softmax(self.affine(z))
+
+class PriorMu(nn.Module):
+    def __init__(self, vocab_size, embed_size):
+        super(PriorMu, self).__init__()
+        self.emb = nn.Embedding(vocab_size, embed_size)
+
+    def forward(self, word):
+        return self.emb(word).squeeze()
+
+class PriorSigma(nn.Module):
+    def __init__(self, vocab_size, embed_size):
+        super(PriorSigma, self).__init__()
+        self.emb = nn.Embedding(vocab_size, embed_size)
+        self.softplus = nn.Softplus()
+
+    def forward(self, word):
+        return self.softplus(self.emb(word)).squeeze()
+
+class EmbedAlignEncoder(nn.Module):
+    def __init__(self, vocab_size, embed_size):
+        super(EmbedAlignEncoder, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.lstm = torch.nn.LSTM(embed_size, embed_size, bidirectional = True, batch_first=True)
+        self.mu_fc1 = nn.Linear(embed_size, embed_size)
+        self.mu_fc2 = nn.Linear(embed_size, embed_size)
+        self.sigma_fc1 = nn.Linear(embed_size, embed_size)
+        self.sigma_fc2 = nn.Linear(embed_size, embed_size)
+        self.relu = nn.ReLU()
+        self.softplus = nn.Softplus()
+        self.embed_size = embed_size
+
+    def forward(self, sentence, lengths):
+        emb = self.embeddings(sentence)
+        packed_emb = pack_padded_sequence(emb, lengths, batch_first=True)
+        packed_lstm_out, (hn, cn) = self.lstm(packed_emb)
+        lstm_out, _ = pad_packed_sequence(packed_lstm_out, batch_first=True)
+        lstm_1, lstm_2 = torch.split(lstm_out, self.embed_size, dim=2)
+        lstm_out = lstm_1 + lstm_2
+        mus = self.mu_fc1(lstm_out)
+        mus = self.relu(mus)
+        mus = self.mu_fc1(mus)
+        sigmas = self.sigma_fc1(lstm_out)
+        sigmas = self.relu(sigmas)
+        sigmas = self.sigma_fc2(sigmas)
+        sigmas = self.softplus(sigmas)
+
+        return mus, sigmas
+
+class EmbedAlignDecoder(nn.Module):
+    def __init__(self, vocab_size, embed_size):
+        super(EmbedAlignDecoder, self).__init__()
+        self.fc1 = nn.Linear(embed_size, embed_size)
+        self.fc2 = nn.Linear(embed_size, vocab_size)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, z):
+        out = self.fc1(z)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return self.softmax(out)
+
+class EmbedAlignELBO(nn.Module):
+    def __init__(self, embed_size):
+        super(EmbedAlignELBO, self).__init__()
+        self.embed_size = embed_size
+
+    def forward(self, sentence_en, sentence_fr, m, mus, sigmas, decoded_en, decoded_fr):
+        sentence_en = sentence_en.unsqueeze(2)
+        sum = decoded_en.gather(2, sentence_en).log().sum()
+
+        for batch in range(decoded_fr.size(0)):
+            data = torch.index_select(decoded_fr[batch], 1, sentence_fr[batch]) / m[batch].float()
+            sum  += data.sum(0).log().sum()
+
+        kl = ((1 / sigmas).log() + (sigmas.pow(2) + mus.pow(2))/2 - 0.5).sum()
+
+        return kl - sum
+
+class ELBO(nn.Module):
+    def __init__(self, embed_size):
+        super(ELBO, self).__init__()
+        self.embed_size = embed_size
+
+    def forward(self, context, mu_lambda, sigma_lambda, mu_x, sigma_x, decoded):
+        sum = decoded.gather(1, context).log().sum()
+        kl = ((sigma_x / sigma_lambda).log() + (sigma_lambda.pow(2) + (mu_lambda-mu_x).pow(2))/(2*sigma_x.pow(2)) - 0.5).sum()
+        return kl - sum
